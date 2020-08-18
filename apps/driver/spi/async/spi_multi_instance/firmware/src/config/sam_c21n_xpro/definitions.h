899de11871485347fa0b5f03e82cd929c66d1c93
Vishal Gandhi <vishal.gandhi@microchip.com>
2020-08-18 14:59:22 +0530

    [DRV_SPI] Regenerated demo for driver_spi_async_spi_multi_instance_sam_c21n_xpro
    MH3-41985
    
    Signed-off-by: Vishal Gandhi <vishal.gandhi@microchip.com>

--- apps/driver/spi/async/spi_multi_instance/firmware/src/config/sam_c21n_xpro/definitions.h
+++ apps/driver/spi/async/spi_multi_instance/firmware/src/config/sam_c21n_xpro/definitions.h
@@ -49,13 +49,13 @@
 #include <stddef.h>
 #include <stdbool.h>
 #include "peripheral/nvmctrl/plib_nvmctrl.h"
-#include "peripheral/sercom/spim/plib_sercom1_spi.h"
+#include "peripheral/sercom/spi_master/plib_sercom1_spi_master.h"
 #include "peripheral/evsys/plib_evsys.h"
 #include "peripheral/port/plib_port.h"
 #include "peripheral/clock/plib_clock.h"
 #include "peripheral/nvic/plib_nvic.h"
 #include "peripheral/dmac/plib_dmac.h"
-#include "peripheral/sercom/spim/plib_sercom5_spi.h"
+#include "peripheral/sercom/spi_master/plib_sercom5_spi_master.h"
 #include "driver/spi/drv_spi.h"
 #include "system/int/sys_int.h"
 #include "system/ports/sys_ports.h"
