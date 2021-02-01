0c2dc8520e593325a853c4d6a005a5e9f8cb68d3
Vishal Gandhi <vishal.gandhi@microchip.com>
2021-02-01 10:58:03 +0530

    [DRV_USART] Regenerated demo for driver_usart_async_usart_multi_instance_sam_c21n_xpro
    MH3-50346
    
    Signed-off-by: Vishal Gandhi <vishal.gandhi@microchip.com>

--- apps/driver/usart/async/usart_multi_instance/firmware/src/config/sam_c21n_xpro/initialization.c
+++ apps/driver/usart/async/usart_multi_instance/firmware/src/config/sam_c21n_xpro/initialization.c
@@ -91,7 +91,7 @@ const DRV_USART_PLIB_INTERFACE drvUsart1PlibAPI = {
     .read = (DRV_USART_PLIB_READ)SERCOM2_USART_Read,
     .readIsBusy = (DRV_USART_PLIB_READ_IS_BUSY)SERCOM2_USART_ReadIsBusy,
     .readCountGet = (DRV_USART_PLIB_READ_COUNT_GET)SERCOM2_USART_ReadCountGet,
-	.readAbort = (DRV_USART_PLIB_READ_ABORT)SERCOM2_USART_ReadAbort,
+    .readAbort = (DRV_USART_PLIB_READ_ABORT)SERCOM2_USART_ReadAbort,
     .writeCallbackRegister = (DRV_USART_PLIB_WRITE_CALLBACK_REG)SERCOM2_USART_WriteCallbackRegister,
     .write = (DRV_USART_PLIB_WRITE)SERCOM2_USART_Write,
     .writeIsBusy = (DRV_USART_PLIB_WRITE_IS_BUSY)SERCOM2_USART_WriteIsBusy,
@@ -143,6 +143,8 @@ const DRV_USART_INIT drvUsart1InitData =
     .remapStopBits = drvUsart1remapStopBits,
 
     .remapError = drvUsart1remapError,
+
+    .dataWidth = DRV_USART_DATA_8_BIT,
 };
 
 // </editor-fold>
@@ -158,7 +160,7 @@ const DRV_USART_PLIB_INTERFACE drvUsart0PlibAPI = {
     .read = (DRV_USART_PLIB_READ)SERCOM4_USART_Read,
     .readIsBusy = (DRV_USART_PLIB_READ_IS_BUSY)SERCOM4_USART_ReadIsBusy,
     .readCountGet = (DRV_USART_PLIB_READ_COUNT_GET)SERCOM4_USART_ReadCountGet,
-	.readAbort = (DRV_USART_PLIB_READ_ABORT)SERCOM4_USART_ReadAbort,
+    .readAbort = (DRV_USART_PLIB_READ_ABORT)SERCOM4_USART_ReadAbort,
     .writeCallbackRegister = (DRV_USART_PLIB_WRITE_CALLBACK_REG)SERCOM4_USART_WriteCallbackRegister,
     .write = (DRV_USART_PLIB_WRITE)SERCOM4_USART_Write,
     .writeIsBusy = (DRV_USART_PLIB_WRITE_IS_BUSY)SERCOM4_USART_WriteIsBusy,
@@ -210,6 +212,8 @@ const DRV_USART_INIT drvUsart0InitData =
     .remapStopBits = drvUsart0remapStopBits,
 
     .remapError = drvUsart0remapError,
+
+    .dataWidth = DRV_USART_DATA_8_BIT,
 };
 
 // </editor-fold>
@@ -258,6 +262,7 @@ SYSTEM_OBJECTS sysObj;
 
 void SYS_Initialize ( void* data )
 {
+
     NVMCTRL_REGS->NVMCTRL_CTRLB = NVMCTRL_CTRLB_RWS(3);
 
   
@@ -271,6 +276,8 @@ void SYS_Initialize ( void* data )
     NVMCTRL_Initialize( );
 
     SERCOM2_USART_Initialize();
+
+    EVSYS_Initialize();
 
     SERCOM4_USART_Initialize();
 
