9f0b5f82288e7595b18331c1635692ac530bbb0d
Vishal Gandhi <vishal.gandhi@microchip.com>
2021-02-01 10:56:48 +0530

    [DRV_USART] Regenerated demo for driver_usart_async_usart_echo_sam_c21n_xpro
    MH3-50345
    
    Signed-off-by: Vishal Gandhi <vishal.gandhi@microchip.com>

--- apps/driver/usart/async/usart_echo/firmware/src/config/sam_c21n_xpro/definitions.h
+++ apps/driver/usart/async/usart_echo/firmware/src/config/sam_c21n_xpro/definitions.h
@@ -49,6 +49,7 @@
 #include <stddef.h>
 #include <stdbool.h>
 #include "peripheral/nvmctrl/plib_nvmctrl.h"
+#include "peripheral/evsys/plib_evsys.h"
 #include "driver/usart/drv_usart.h"
 #include "peripheral/port/plib_port.h"
 #include "peripheral/clock/plib_clock.h"
@@ -70,6 +71,9 @@ extern "C" {
 
 #endif
 // DOM-IGNORE-END
+
+/* CPU clock frequency */
+#define CPU_CLOCK_FREQUENCY 48000000
 
 // *****************************************************************************
 // *****************************************************************************
@@ -163,22 +167,22 @@ void SYS_Tasks ( void );
 // Section: Type Definitions
 // *****************************************************************************
 // *****************************************************************************
-    
+
 // *****************************************************************************
 /* System Objects
-        
+
 Summary:
     Structure holding the system's object handles
-        
+
 Description:
     This structure contains the object handles for all objects in the
     MPLAB Harmony project's system configuration.
-        
+
 Remarks:
     These handles are returned from the "Initialize" functions for each module
     and must be passed into the "Tasks" function for each module.
 */
-        
+
 typedef struct
 {
     SYS_MODULE_OBJ  drvUsart0;
