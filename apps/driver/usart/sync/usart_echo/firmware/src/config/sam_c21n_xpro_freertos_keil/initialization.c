3d292ba4d7bbd68fae899a5c3d7a69680e1de619
Jignesh Moradiya <jignesh.moradiya@microchip.com>
2021-08-13 12:15:07 +0530

    [DRV_USART] Regenerated demo for driver_usart_sync_usart_echo_sam_c21n_xpro_freertos_keil
    MH3-56185
    
    Signed-off-by: Jignesh Moradiya <jignesh.moradiya@microchip.com>

--- apps/driver/usart/sync/usart_echo/firmware/src/config/sam_c21n_xpro_freertos_keil/initialization.c
+++ apps/driver/usart/sync/usart_echo/firmware/src/config/sam_c21n_xpro_freertos_keil/initialization.c
@@ -96,10 +96,6 @@ const DRV_USART_INIT drvUsart0InitData =
     /* USART Client Objects Pool */
     .clientObjPool = (uintptr_t)&drvUSART0ClientObjPool[0],
 
-    .dmaChannelTransmit = SYS_DMA_CHANNEL_NONE,
-
-    .dmaChannelReceive = SYS_DMA_CHANNEL_NONE,
-
 
     .remapDataWidth = drvUsart0remapDataWidth,
 
@@ -159,7 +155,7 @@ SYSTEM_OBJECTS sysObj;
 void SYS_Initialize ( void* data )
 {
 
-    NVMCTRL_REGS->NVMCTRL_CTRLB = NVMCTRL_CTRLB_RWS(3);
+    NVMCTRL_REGS->NVMCTRL_CTRLB = NVMCTRL_CTRLB_RWS(3UL);
 
   
     PORT_Initialize();
