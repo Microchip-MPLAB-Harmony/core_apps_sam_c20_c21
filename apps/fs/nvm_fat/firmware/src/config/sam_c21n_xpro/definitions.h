581a0bcec97cced5b724f08298af4eed12efd12a
Vishal Gandhi <vishal.gandhi@microchip.com>
2022-05-13 10:27:43 +0530

    [SYS_FS] Regenerated demo for fs_nvm_fat_sam_c21n_xpro
    MH3-65229
    
    Signed-off-by: Vishal Gandhi <vishal.gandhi@microchip.com>

--- apps/fs/nvm_fat/firmware/src/config/sam_c21n_xpro/definitions.h
+++ apps/fs/nvm_fat/firmware/src/config/sam_c21n_xpro/definitions.h
@@ -61,10 +61,10 @@
 #include "peripheral/clock/plib_clock.h"
 #include "peripheral/nvic/plib_nvic.h"
 #include "driver/memory/drv_memory.h"
+#include "bsp/bsp.h"
 #include "system/int/sys_int.h"
 #include "osal/osal.h"
 #include "system/debug/sys_debug.h"
-#include "bsp/bsp.h"
 #include "app.h"
 
 
