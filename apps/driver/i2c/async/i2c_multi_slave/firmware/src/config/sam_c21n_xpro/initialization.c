86bb787e65f75b45da55769e72032fb097145cfe
Vishal Gandhi <vishal.gandhi@microchip.com>
2022-05-13 10:11:39 +0530

    [DRV_I2C] Regenerated demo for driver_i2c_async_i2c_multi_slave_sam_c21n_xpro
    MH3-65214
    
    Signed-off-by: Vishal Gandhi <vishal.gandhi@microchip.com>

--- apps/driver/i2c/async/i2c_multi_slave/firmware/src/config/sam_c21n_xpro/initialization.c
+++ apps/driver/i2c/async/i2c_multi_slave/firmware/src/config/sam_c21n_xpro/initialization.c
@@ -100,6 +100,9 @@ const DRV_I2C_PLIB_INTERFACE drvI2C0PLibAPI = {
     /* I2C PLib Transfer Write Read Add function */
     .writeRead = (DRV_I2C_PLIB_WRITE_READ)SERCOM1_I2C_WriteRead,
 
+    /*I2C PLib Tranfer Abort function */
+    .transferAbort = (DRV_I2C_PLIB_TRANSFER_ABORT)SERCOM1_I2C_TransferAbort,
+
     /* I2C PLib Transfer Status function */
     .errorGet = (DRV_I2C_PLIB_ERROR_GET)SERCOM1_I2C_ErrorGet,
 
