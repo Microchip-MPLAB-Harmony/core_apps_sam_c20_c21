7eb2916bd2dd329d14ebc6c8e653de2215389216
Vishal Gandhi <vishal.gandhi@microchip.com>
2022-05-13 10:25:14 +0530

    [DRV_USART] Regenerated demo for driver_usart_sync_usart_echo_sam_c21n_xpro_freertos_keil
    MH3-65227
    
    Signed-off-by: Vishal Gandhi <vishal.gandhi@microchip.com>

--- apps/driver/usart/sync/usart_echo/firmware/src/config/sam_c21n_xpro_freertos_keil/interrupts.c
+++ apps/driver/usart/sync/usart_echo/firmware/src/config/sam_c21n_xpro_freertos_keil/interrupts.c
@@ -72,6 +72,9 @@ void __attribute__((section(".text.Dummy_Handler")))Dummy_Handler(void)
     {
     }
 }
+
+/* MISRAC 2012 deviation block start */
+/* MISRA C-2012 Rule 8.6 deviated 32 times.  Deviation record ID -  H3_MISRAC_2012_R_8_6_DR_1 */
 /* Device vectors list dummy definition*/
 extern void vPortSVCHandler            ( void ) __attribute__((weak, alias("Dummy_Handler")));
 extern void xPortPendSVHandler         ( void ) __attribute__((weak, alias("Dummy_Handler")));
@@ -107,6 +110,7 @@ extern void SDADC_Handler              ( void ) __attribute__((weak, alias("Dumm
 extern void PTC_Handler                ( void ) __attribute__((weak, alias("Dummy_Handler")));
 
 
+/* MISRAC 2012 deviation block end */
 
 /* Multiple handlers for vector */
 
