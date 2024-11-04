01877e845000cc8676c9c32bdee7e4c0c40b2987
Pranjal Das <pranjal.das@microchip.com>
2024-11-04 15:10:56 +0530

    [DRV_SDSPI] Regenerated demo for driver__sdspi__sync__sdspi_read_write__firmware__sam_c21n_xpro_freertos.X
    MH3-103648

--- apps/driver/sdspi/sync/sdspi_read_write/firmware/src/config/sam_c21n_xpro_freertos/definitions.h
+++ apps/driver/sdspi/sync/sdspi_read_write/firmware/src/config/sam_c21n_xpro_freertos/definitions.h
@@ -60,12 +60,12 @@
 #include "system/time/sys_time.h"
 #include "FreeRTOS.h"
 #include "task.h"
+#include "bsp/bsp.h"
 #include "system/int/sys_int.h"
 #include "system/ports/sys_ports.h"
 #include "system/dma/sys_dma.h"
 #include "osal/osal.h"
 #include "system/debug/sys_debug.h"
-#include "bsp/bsp.h"
 #include "app.h"
 
 
@@ -79,13 +79,13 @@ extern "C" {
 // DOM-IGNORE-END
 
 /* Device Information */
-#define DEVICE_NAME			 "ATSAMC21N18A"
-#define DEVICE_ARCH			 "CORTEX-M0PLUS"
-#define DEVICE_FAMILY		 "SAMC"
-#define DEVICE_SERIES		 "SAMC21"
+#define DEVICE_NAME          "ATSAMC21N18A"
+#define DEVICE_ARCH          "CORTEX-M0PLUS"
+#define DEVICE_FAMILY        "SAMC"
+#define DEVICE_SERIES        "SAMC21"
 
 /* CPU clock frequency */
-#define CPU_CLOCK_FREQUENCY 48000000
+#define CPU_CLOCK_FREQUENCY 48000000U
 
 // *****************************************************************************
 // *****************************************************************************
